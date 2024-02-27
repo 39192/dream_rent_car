8187f1354608ef9bb0cbdbac024a68bc1f4d092c
choihansu97 <jjpp9712@naver.com>
2024-02-27 17:54:29 +0900

    fix(index) : 경로문제

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import './js/jquery-3.4.1.min.js'
-import './js/guamfree-dream-calculator.min.js'
-import './js/bootstrap-datepicker.js'
-import './css/bootstrap-datepicker.css'
-import './css/guamfree-dream-calculator.css'
 
 createApp(App).mount('#app')
