6f82c037a306059ac2d6f1aa5035c8a69cdc3870
choihansu97 <jjpp9712@naver.com>
2024-02-28 10:29:09 +0900

    fix(side): 주석처리 및 오류수정

--- src/components/SideOption.vue
+++ src/components/SideOption.vue
@@ -14,24 +14,48 @@
                     <div class="list-group-item">
                         <h6>픽업일자</h6>
                         <div class="input-group date pickup-date">
-                            <input class="form-control input-group-addon" id="pickup-date" readonly="">
+                            <input class="form-control input-group-addon" id="pickup-date"
+                                   readonly="">
                         </div>
                     </div>
                     <div class="list-group-item">
                         <h6>픽업시간</h6>
                         <select class="form-control" id="pickup-time">
-                            <option value="08:00">08:00</option><option value="08:30">08:30</option><option value="09:00">09:00</option><option value="09:30">09:30</option><option value="10:00" selected="">10:00</option><option value="10:30">10:30</option><option value="11:00">11:00</option><option value="11:30">11:30</option><option value="12:00">12:00</option><option value="12:30">12:30</option><option value="13:00">13:00</option><option value="13:30">13:30</option><option value="14:00">14:00</option><option value="14:30">14:30</option><option value="15:00">15:00</option><option value="15:30">15:30</option><option value="16:00">16:00</option><option value="16:30">16:30</option><option value="17:00">17:00</option><option value="17:30">17:30</option><option value="18:00">18:00</option><option value="18:30">18:30</option></select>
+                            <option value="08:00">08:00</option>
+                            <option value="08:30">08:30</option>
+                            <option value="09:00">09:00</option>
+                            <option value="09:30">09:30</option>
+                            <option value="10:00" selected="">10:00</option>
+                            <option value="10:30">10:30</option>
+                            <option value="11:00">11:00</option>
+                            <option value="11:30">11:30</option>
+                            <option value="12:00">12:00</option>
+                            <option value="12:30">12:30</option>
+                            <option value="13:00">13:00</option>
+                            <option value="13:30">13:30</option>
+                            <option value="14:00">14:00</option>
+                            <option value="14:30">14:30</option>
+                            <option value="15:00">15:00</option>
+                            <option value="15:30">15:30</option>
+                            <option value="16:00">16:00</option>
+                            <option value="16:30">16:30</option>
+                            <option value="17:00">17:00</option>
+                            <option value="17:30">17:30</option>
+                            <option value="18:00">18:00</option>
+                            <option value="18:30">18:30</option>
+                        </select>
                     </div>
                     <div class="list-group-item">
                         <h6>반납일자</h6>
                         <div class="input-group date return-date">
-                            <input class="form-control input-group-addon" id="return-date" readonly="">
+                            <input class="form-control input-group-addon" id="return-date"
+                                   readonly="">
                         </div>
                     </div>
                     <div class="list-group-item">
                         <h6>반납시간</h6>
                         <select class="form-control" id="return-time">
-                            <option value="00:00">00:00</option><option value="00:30">00:30</option><option value="01:00">01:00</option><option value="01:30">01:30</option><option value="02:00">02:00</option><option value="02:30">02:30</option><option value="08:00">08:00</option><option value="08:30">08:30</option><option value="09:00">09:00</option><option value="09:30">09:30</option><option value="10:00" selected="">10:00</option><option value="10:30">10:30</option><option value="11:00">11:00</option><option value="11:30">11:30</option><option value="12:00">12:00</option><option value="12:30">12:30</option><option value="13:00">13:00</option><option value="13:30">13:30</option><option value="14:00">14:00</option><option value="14:30">14:30</option><option value="15:00">15:00</option><option value="15:30">15:30</option><option value="16:00">16:00</option><option value="16:30">16:30</option><option value="17:00">17:00</option><option value="17:30">17:30</option><option value="18:00">18:00</option><option value="18:30">18:30</option><option value="19:00">19:00</option><option value="19:30">19:30</option><option value="20:00">20:00</option><option value="20:30">20:30</option><option value="21:00">21:00</option><option value="21:30">21:30</option><option value="22:00">22:00</option><option value="22:30">22:30</option><option value="23:00">23:00</option><option value="23:30">23:30</option></select>
+                        </select>
                     </div>
                     <div class="list-group-item" style="background-color: #F5F5F5">
                         <div>렌트기간</div>
@@ -51,10 +75,14 @@
                     * 견적계산기는 렌트 비용 참고용입니다.
                 </div>
                 <div class="info-sentence">
-                    * 자세한 내용은 <b><a href="https://cafe.naver.com/guamfree/2593378" style="color: rgb(107, 144, 220)" target="_blank"><u>드림렌트카 예약안내</u></a></b> 에서 확인해주세요.
+                    * 자세한 내용은 <b><a href="https://cafe.naver.com/guamfree/2593378"
+                                    style="color: rgb(107, 144, 220)" target="_blank"><u>드림렌트카
+                    예약안내</u></a></b> 에서 확인해주세요.
                 </div>
                 <div class="info-sentence">
-                    * 렌트기간 7일, 30일 이상의 장기 렌트일 경우 메일로 별도 문의 바랍니다. &lt; <b><a href="mailto:tillpark3@naver.com" style="color: rgb(107, 144, 220)" target="_blank"><u>tillpark3@naver.com</u></a></b> &gt;
+                    * 렌트기간 7일, 30일 이상의 장기 렌트일 경우 메일로 별도 문의 바랍니다. &lt; <b><a
+                    href="mailto:tillpark3@naver.com" style="color: rgb(107, 144, 220)"
+                    target="_blank"><u>tillpark3@naver.com</u></a></b> &gt;
                 </div>
 
                 <br>
