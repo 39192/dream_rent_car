00373aadf4374a968814286fff5d00e4df023596
39192 <160730385+39192@users.noreply.github.com>
2024-03-04 10:02:02 +0900

    Update SideOption.vue

--- src/components/SideOption.vue
+++ src/components/SideOption.vue
@@ -57,7 +57,7 @@
                 <div class="info-sentence">
                     * 장기 할인안내 <b><a href="https://cafe.naver.com/guamfree/2410415"
                                     style="color: rgb(107, 144, 220)" target="_blank"><u>7일</u></a></b> <b><a href="https://cafe.naver.com/guamfree/2372137"
-                                    style="color: rgb(107, 144, 220)" target="_blank"><u>30</u></a></b>
+                                    style="color: rgb(107, 144, 220)" target="_blank"><u>30일</u></a></b>
                 </div>
                 <div class="info-sentence">
                     * 추가 문의는 메일로 송부바랍니다. &lt; <b><a
